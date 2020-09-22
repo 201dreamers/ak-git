5cd7795a48927269fd6c6f63a1805be63bde571c
201dreamers <201dreamers@gmail.com>
2020-09-22 21:46:05 +0300

    My own commit (5 - 15 changes)

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+//Add function
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//Substitution function
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//Multiplication function
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
+}
+
+//Division function
+int Calculator::Div (double a, double b)
+{
+    return a / b;
 }
